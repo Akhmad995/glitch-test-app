c1c476cd41bea94e85ed75386e23ce14ab14e0d6
Akhmad995 <axmed-005@mail.ru>
2023-12-30 20:46:31 +0300

    добавил исполняемый файл

---
+++ start.sh
@@ -0,0 +1,18 @@
+set -eu
+
+VIRTUALENV=./venv
+
+if [ ! -d $VIRTUALENV ]; then
+    python3 -m venv $VIRTUALENV
+fi
+
+
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+fi
+
+
+$VIRTUALENV/bin/pip innstal -r requirements.txt
+
+
+$VIRTUALENV/bin/python3 wsgi.py
